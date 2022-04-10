bbe9792c5af5090b7ed501162901c5d8852ec388
yasmine <yasmine.mmarbani2001@gmail.com>
2022-04-11 01:48:51 +0300

    edit nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./Nav.css";
 import { AiOutlineHome } from "react-icons/ai";
@@ -7,56 +7,63 @@ import { BiBook } from "react-icons/bi";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import { AiOutlineAppstore } from "react-icons/ai";
 import { useState } from "react";
-import {MdExpandMore} from 'react-icons/md';
-import {MdClose} from 'react-icons/md';
+import { MdExpandMore } from "react-icons/md";
+import { MdClose } from "react-icons/md";
 
 const Nav = () => {
   const [active, setActive] = useState("#");
-  const[off,setOff]=useState(true);
+  const [off, setOff] = useState(true);
   return (
-    <nav className={off ?  "offN" :"" }>
+    <nav
+      className={off ? "offN" : ""}
+      onMouseEnter={() => setOff(false)}
+      onMouseLeave={() => setOff(true)}
+    >
       <a
         href="#"
-        className={off ? "off" :active === "#" ? "active" : ""}
+        className={off ? "off" : active === "#" ? "active" : ""}
         onClick={() => setActive("#")}
       >
         <AiOutlineHome />
       </a>
       <a
         href="#about"
-        className={off ? "off" :active === "#about" ? "active" : ""}
+        className={off ? "off" : active === "#about" ? "active" : ""}
         onClick={() => setActive("#about")}
       >
         <AiOutlineUser />
       </a>
       <a
         href="#experience"
-        className={off ? "off" :active === "#experience" ? "active" : ""}
+        className={off ? "off" : active === "#experience" ? "active" : ""}
         onClick={() => setActive("#experience")}
       >
         <BiBook />
       </a>
       <a
         href="#portfolio"
-        className={off ? "off" :active === "#portfolio" ? "active" : ""}
+        className={off ? "off" : active === "#portfolio" ? "active" : ""}
         onClick={() => setActive("#portfolio")}
       >
         <AiOutlineAppstore />
       </a>
       <a
         href="#contact"
-        className={off ? "off" :active === "#contact" ? "active" : ""}
+        className={off ? "off" : active === "#contact" ? "active" : ""}
         onClick={() => setActive("#contact")}
       >
         <BiMessageSquareDetail />
       </a>
-      <a className='nav-btn'
-      onClick={() => setOff(!off)}>
-        {off?<MdExpandMore />:<MdClose />}
+      <a
+        className="nav-btn"
+        onClick={() => {
+          setOff(!off);
+        }}
+      >
+        {off ? <MdExpandMore /> : <MdClose />}
       </a>
     </nav>
+  );
+};
 
-  )
-}
-
-export default Nav
+export default Nav;
