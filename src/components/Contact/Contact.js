87a4d7f3d86cd6ce0a72304cd5314d5351831480
yasmine <yasmine.mmarbani2001@gmail.com>
2022-04-12 23:33:38 +0300

    finish footer(everything)

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import { BsMessenger } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_zkftv8e",
+      "template_sxx5xqj",
+      form.current,
+      "YOpulPuosbcjPAXX2"
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <BsMessenger className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Yasmine Maarbani</h5>
+            <a href="https://m.me/Yasmine.Maarbani" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsInstagram className="contact__option-icon" />
+            <h4>Instagram</h4>
+            <h5>Yasmine Maarbani</h5>
+            <a
+              href="https://www.instagram.com/yasmine.maarbani/"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+96181231448</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+96181231448"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="" ref={form} onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="name" required />
+          <input type="email" name="email" placeholder="email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="your message"
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import "./Footer.css";
+import { BsInstagram } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Yasmine
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://facebook.com/Yasmine.Maarbani"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://instagram.com/yasmine.maarbani"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+          <IoLogoTwitter />
+        </a>
+        <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/yasmine-maarbani"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Yasmine Maarbani. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
